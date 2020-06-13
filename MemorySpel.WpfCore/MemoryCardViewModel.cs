69be24cb4202d19332c86bed76b8e7e466d7589a
mseress <a@b.com>
2020-06-13 21:16:17 +0200

    Adding view model classes.

---
+++ MemorySpel.WpfCore/MemoryCardViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace MemorySpel.WpfCore
+{
+    public class MemoryCardViewModel : ViewModel
+    {
+        private double _height;
+
+        public double Height
+        {
+            get { return _height; }
+            set 
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                    this.OnPropertyChanged();
+                }                
+            }
+        }
+
+        private double _width;
+
+        public double Width
+        {
+            get { return _width; }
+            set 
+            {
+                if (_width != value)
+                {
+                    _width = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public MemoryCardViewModel()
+        {            
+            this.Height = 150;
+            this.Width = 200;
+        }
+    }
+}
---
+++ MemorySpel.WpfCore/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MemorySpel.WpfCore
+{
+    /// <summary>
+    /// Abstract class to be used for view model classes. Implements <see cref="INotifyPropertyChanged"/>.
+    /// </summary>
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is called when a property is changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Method to be called when a property changes so that the UI thread can be notified.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that just changed.</param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
