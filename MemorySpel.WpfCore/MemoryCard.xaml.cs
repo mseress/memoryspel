6ac2be08747fce2a59e6a1a2d65454779ba0772a
mseress <a@b.com>
2020-06-13 21:16:05 +0200

    Removing card_back.png. Properly styling MemoryCard.

--- MemorySpel.WpfCore/MemoryCard.xaml.cs
+++ MemorySpel.WpfCore/MemoryCard.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace MemorySpel.WpfCore
 {
     /// <summary>
     /// Interaction logic for MemoryCard.xaml
     /// </summary>
-    public partial class MemoryCard : Button
+    public partial class MemoryCard : Border
     {
         public MemoryCard()
         {
